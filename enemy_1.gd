1700eec0f27e49c818bfdb09f982e578fa529447
AliYa <enderguy520@gmail.com>
2026-04-06 08:28:12 +0700

    1

--- enemy_1.gd
+++ enemy_1.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
 @export var Speed = 300;
-
+@export var Health = 100;
 
 func _on_PlayerDetectionArea_entered() :
 	pass
---
+++ enemy_2.gd
@@ -0,0 +1,11 @@
+extends CharacterBody2D
+
+@export var Speed = 300;
+@export var Health = 100;
+
+func _on_PlayerDetectionArea_entered() :
+	pass
+	
+func _on_PlayerDetectionArea_exited() :
+	pass
+	
